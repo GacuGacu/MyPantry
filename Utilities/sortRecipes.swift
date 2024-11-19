1c19dbf32a2b5ba77d58e09e55190cebc62b59fa
GacuGacu <91371291+GacuGacu@users.noreply.github.com>
2024-11-19 21:50:12 +0100

    Create sortRecipes.swift

---
+++ Utilities/sortRecipes.swift
@@ -0,0 +1,25 @@
+//
+//  sortRecipe.swift
+//  MyPantry
+//
+//  Created by Kacper Domagała on 18/11/2024.
+//
+
+import Foundation
+
+// Sort recipes to ensure full recipes come first.
+func sortRecipesByPantryMatch(recipes: [Recipe], pantryList: [PantryIngredient]) -> [Recipe] {
+    return recipes.sorted { recipe1, recipe2 in
+        let recipe1MissingIngredients = recipe1.ingredients.filter { ingredient in
+            !pantryList.contains { pantryItem in
+                pantryItem.name.lowercased() == ingredient.lowercased()
+            }
+        }
+        let recipe2MissingIngredients = recipe2.ingredients.filter { ingredient in
+            !pantryList.contains { pantryItem in
+                pantryItem.name.lowercased() == ingredient.lowercased()
+            }
+        }
+        return recipe1MissingIngredients.isEmpty && !recipe2MissingIngredients.isEmpty
+    }
+}
